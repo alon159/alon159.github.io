3ca21b297e3af0c42bbfbdb819b1f9c8a310a9d5
Alonso Crespo <aloncrespfer@gmail.com>
2025-08-23 00:40:21 +0200

    Added google verification tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
     description: 'Soy Alonso Crespo y este es mi portfolio! Aquí podrás encontrar información sobre mi experiencia y mis proyectos realizados como Ingeniero Informático.',
     author: 'Alonso Crespo Fernández',
     keywords: 'Alonso Crespo Fernández, Alonso Crespo Fernandez, alonso.crespo, Ingeniero Informático, UCLM, Web, Ciudad Real, ESI, Escuela Superior de Informática, Tecnología, Información',
+    verification: {
+        google: '_iFFSbxa_QptjLCfBXKlAFrhr30zjgJrXxWJihbM0xo', // Add your verification code
+    },
 }
 
 export default function RootLayout({ children }) {
